fa904d070c481369c9464c3c016eae2a46fe3626
wathers <chenhao2103@163.com>
2022-12-04 15:22:04 +0800

    fisrt commit

---
+++ easydata-server/src/main/java/com/chen/easydata/server/EasyDataServerApplication.java
@@ -0,0 +1,11 @@
+package com.chen.easydata.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDataServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasyDataServerApplication.class, args);
+    }
+}
